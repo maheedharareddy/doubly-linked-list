429cceace707baa98b265d11098d5b33d7eafadd
Maheedhara Reddy Kakarla <Maheedhara_Reddy_Kakarla@epam.com>
2017-03-15 18:16:11 +0530

    test finished

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,123 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        var list=new Array();
+        list.append =this.append;
+        list.tail=this.tail;
+        list.head=this.head;
+        list.at=this.at;
+        list.isEmpty=this.isEmpty;
+        list.at=this.at;
+        list.indexOf=this.indexOf;
+        list.clear=this.clear;
+        list.insertAt=this.insertAt;
+        list.deleteAt=this.deleteAt;
+        list._head= new Node();
+        list._tail=new Node();
 
-    append(data) {}
 
-    head() {}
+       return list;
+    }
 
-    tail() {}
+    append(data) {
+     
+        if(data !=undefined)        
+        {
 
-    at(index) {}
+         this.push(data);
+         if(this.length == 1)
+            {
+                this._head=new Node(this.head());
+                this._tail=new Node(this.tail());
+            }
+         }
+        return this;
+    }
 
-    insertAt(index, data) {}
+    head() {
+        if(this.length == 0)
+        {
+            return null;
+        }
 
-    isEmpty() {}
+        return this.at(0);
+     
+    }
 
-    clear() {}
 
-    deleteAt(index) {}
+    tail() {
+        if(this.length == 0)
+        {
+            return null;
+        }
+        return this[this.length-1];
+       
+    }
 
-    reverse() {}
+    at(index) {
 
-    indexOf(data) {}
+     if(this != null && this != undefined && this.length >= index)
+        {
+            return this[index];
+        }
+        return null;
+
+    }
+
+    insertAt(index, data) {
+        if(this != null && this != undefined && data !=undefined)
+        {
+            this.splice(index,0,data);
+        }
+
+    }
+
+    isEmpty() {
+        if(this != null && this != undefined && this.length == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    clear() { 
+       this.length=0;
+       return this;
+    }
+
+
+
+    deleteAt(index) {
+
+      if(this != null && this != undefined && this.length > 0)        
+        {
+           this.splice(index,1);
+        }
+        return this;
+    }
+
+    reverse() {
+        
+    }
+
+    indexOf(data) {
+
+        
+        if(this != null && this != undefined)        
+            {
+                for (var i=0; i<  this.length ; i++) {
+                    if( this[i] == data )
+                        {
+                            return i;
+                            break;
+                         }
+                }
+            }
+        return -1;
+
+    }
 }
 
 module.exports = LinkedList;
